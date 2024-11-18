96e98a7fd01804d91facaaf6c34ea52a25ae923f
harttman <dakiv.offical.vitaliy@gmail.com>
2024-11-18 23:47:32 +0000

    Add: Showing progress for user Fix: Pom.xml

--- src/main/java/inc/master/harttman/App.java
+++ src/main/java/inc/master/harttman/App.java
@@ -12,16 +12,20 @@ public class App
         JFrame frame = new JFrame("Minecraft");
         frame.setLayout(null);
         JButton hello = new JButton("Click"); // text for button
+        JLabel label = new JLabel("Count 0"); // create label for user
 
         hello.setBounds(200, 50, 100, 50); // button position
+        label.setBounds(20, 20, 100, 50);
         hello.addActionListener(e -> {
             count++; // if button clicked == count + 1
             hello.setText("Done.."); // if button clicked == new text
+            label.setText("Count " + count); // showing progress
             System.out.println(count); // logging count in console
         });
 
         frame.getContentPane().setBackground(Color.blue); // set bg color
         frame.add(hello); // add button
+        frame.add(label); // add label
         frame.setSize(800, 600); // size window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close window
         frame.setVisible(true); // visible window
