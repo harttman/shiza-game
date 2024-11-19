cfbdcfc2523d350e37a0f6f9796cec73eddaf1dd
harttman <dakiv.offical.vitaliy@gmail.com>
2024-11-19 14:21:08 +0200

    Change Logics.

--- src/main/java/inc/master/harttman/App.java
+++ src/main/java/inc/master/harttman/App.java
@@ -3,31 +3,27 @@ package inc.master.harttman;
 import javax.swing.*;
 import java.awt.*;
 
-public class App 
-{
-    private static int count = 0; // count click
-    public static void main( String[] args )
-    {
-        // basic classes
-        JFrame frame = new JFrame("Minecraft");
-        frame.setLayout(null);
-        JButton hello = new JButton("Click"); // text for button
-        JLabel label = new JLabel("Count 0"); // create label for user
+public class App {
+    private static int count = 0; // default value
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Clicker | Shiza Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
 
-        hello.setBounds(200, 50, 100, 50); // button position
-        label.setBounds(20, 20, 100, 50);
-        hello.addActionListener(e -> {
-            count++; // if button clicked == count + 1
-            hello.setText("Done.."); // if button clicked == new text
-            label.setText("Count " + count); // showing progress
-            System.out.println(count); // logging count in console
+        JPanel mainPanel = new JPanel(new CardLayout());
+        JPanel workPanel = new JPanel();
+        JLabel labelCount = new JLabel("Count " + count, SwingConstants.CENTER);
+        JButton clickButton = new JButton("Click for make more cash.");
+        workPanel.setLayout(new BorderLayout());
+
+        clickButton.addActionListener(e -> {
+            count++;
+            labelCount.setText("Count: " + count);
         });
 
-        frame.getContentPane().setBackground(Color.blue); // set bg color
-        frame.add(hello); // add button
-        frame.add(label); // add label
-        frame.setSize(800, 600); // size window
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close window
-        frame.setVisible(true); // visible window
+        workPanel.add(labelCount, BorderLayout.CENTER);
+        workPanel.add(clickButton, BorderLayout.SOUTH);
+
+
     }
 }
